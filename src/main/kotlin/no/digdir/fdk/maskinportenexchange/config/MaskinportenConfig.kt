e4080fe3aa48b7a5c56017dce43d473394c0f137
Hassan Alam <hassan@hotmail.no>
2026-04-09 06:58:46 +0200

    Add Spring Retry and AOP dependencies; enhance RestTemplate configuration and implement retry logic in MaskinportenClient

--- src/main/kotlin/no/digdir/fdk/maskinportenexchange/config/MaskinportenConfig.kt
+++ src/main/kotlin/no/digdir/fdk/maskinportenexchange/config/MaskinportenConfig.kt
@@ -2,16 +2,23 @@ package no.digdir.fdk.maskinportenexchange.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.retry.annotation.EnableRetry
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
+@EnableRetry
 class MaskinportenConfig {
 
     @Bean
-    fun restTemplate(): RestTemplate {
-        return RestTemplate()
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder
+            .connectTimeout(Duration.ofSeconds(2))
+            .readTimeout(Duration.ofSeconds(2))
+            .build()
     }
 
     @Bean
